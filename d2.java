c27f4f57b051221b32c297c3fbcafd6ae7932626
Ratul Arora <arora.ratul@gmail.com>
2017-04-20 21:47:44 +0530

    date  format in java

---
+++ d2.java
@@ -0,0 +1,19 @@
+import java.text.*;
+import java.util.*;
+
+ class Main {
+   public static void main(String[] args) {
+      Date dt = new Date();
+      DateFormat[] dtformat = new DateFormat[6];
+      
+      dtformat[0] = DateFormat.getInstance();
+      dtformat[1] = DateFormat.getDateInstance();
+      dtformat[2] = DateFormat.getDateInstance(DateFormat.MEDIUM);
+      dtformat[3] = DateFormat.getDateInstance(DateFormat.FULL);
+      dtformat[4] = DateFormat.getDateInstance(DateFormat.LONG);
+      dtformat[5] = DateFormat.getDateInstance(DateFormat.SHORT);
+      
+      for(DateFormat dateform : dtformat) 
+	  System.out.println(dateform.format(dt));
+   }
+}
